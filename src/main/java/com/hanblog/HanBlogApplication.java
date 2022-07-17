bdc1c6bbe4d3138ed825b0103dbe8a4e1581c031
dygks <dygks34@gmail.com>
2022-07-17 16:02:56 +0900

    환경세팅 완료

---
+++ src/main/java/com/hanblog/HanBlogApplication.java
@@ -0,0 +1,13 @@
+package com.hanblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanBlogApplication.class, args);
+    }
+
+}
